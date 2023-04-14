896079b026ebde0cf880d530fb30ec0e4e84fae4
Franco Cirielli <ciriellifrancogiovanni@gmail.com>
2023-04-13 22:59:08 -0300

    Practica 3

--- Practicas/Practica 2/Punto13/Punto13.pas
+++ Practicas/Practica 2/Punto13/Punto13.pas
@@ -1,5 +1,6 @@
 {
-   Suponga que usted es administrador de un servidor de correo electrónico. En los logs
+
+Suponga que usted es administrador de un servidor de correo electrónico. En los logs
 del mismo (información guardada acerca de los movimientos que ocurren en el server) que
 se encuentra en la siguiente ruta: /var/log/logmail.dat se guarda la siguiente información:
 
@@ -28,13 +29,91 @@ existen en el sistema
 }
 
 
-program untitled;
+program Punto13;
+const VALORALTO=9999;
 
-uses crt;
-var i : byte;
+type maestro=record
+	nro_usuario:integer;
+	nombre_usuario:string;
+	nombre:string;
+	cantidadMailEnviados:integer;
+end;
 
+type detalle=record
+	nro_usuario:integer;
+	cuentaDestino:string;
+	cuerpoMensaje:string;
+end;
+
+archivo_maestro=file of maestro;
+archivo_detalle=file of detalle;
+
+procedure leerD(var d:archivo_detalle;var data:detalle);
+begin
+	if(not eof(d))then
+		read(d,data)
+	else
+		data.nro_usuario:=VALORALTO;
+end;
+
+
+procedure actualizar(var mae:archivo_maestro);
+var
+	m:maestro;
+	detalles:archivo_detalle;
+	d:detalle;
+	nro_actual,mensajes_total:integer;
+begin
+	assign(detalles,'detalle');
+	reset(detalles);
+	reset(mae);
+	leerD(detalles,d);
+	read(mae,m);
+	while(d.nro_usuario<>VALORALTO)do
+	begin
+		nro_actual:=d.nro_usuario;
+		mensajes_total:=0;
+		while(d.nro_usuario=nro_actual)do
+		begin
+			mensajes_total:=mensajes_total+1;
+			leerD(detalles,d);
+		end;
+		read(mae,m);
+		while(m.nro_usuario<>nro_actual)do
+		begin
+			read(mae,m);
+		end;
+		seek(mae,filepos(mae)-1);
+		m.cantidadMailEnviados+=mensajes_total;
+		write(mae,m);
+	end;
+	close(mae);
+	close(detalles);
+end;
+
+procedure exportarTxt(var mae:archivo_maestro);
+var
+	txt:Text;
+	m:maestro;
+begin
+	reset(mae);
+	assign(txt,'info.txt'); 
+	rewrite(txt);
+	read(mae,m);
+	while(not eof(mae))do
+	begin
+		with m do 
+		begin
+			writeln(txt,nro_usuario,'…………',cantidadMailEnviados);
+		end;
+	end;
+end;
+var
+	mae:archivo_maestro;
 BEGIN
-	
-	
+	assign(mae,'maestro');
+	reset(mae);
+	actualizar(mae);
+	exportarTxt(mae);
 END.
 
